dde55817678f9698db821ca6dd45154b15f759ba
Pepi Paraskevoulakou <e.paraskevoulakou@unipi.gr>
2021-11-24 21:29:03 +0200

    changes 13

--- env.py
+++ env.py
@@ -210,7 +210,7 @@ model.summary()
 
 dqn = build_agent(model, actions)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-dqn.fit(my_env, nb_steps=1000, visualize=False, verbose=1)
+dqn.fit(my_env, nb_steps=3, visualize=False, verbose=1)
 
 scores = dqn.test(my_env, nb_episodes=100, visualize=False)
 print(np.mean(scores.history['episode_reward']))
