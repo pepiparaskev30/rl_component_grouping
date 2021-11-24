a74c91b477b94000bd09b6916628c36ef4f94096
Pepi Paraskevoulakou <e.paraskevoulakou@unipi.gr>
2021-11-24 19:51:22 +0200

    new changes 6

--- env.py
+++ env.py
@@ -76,6 +76,22 @@ def cost_calculation(reward, seen_actions):
 
     return reward
 
+
+def build_model(states, actions):
+    model = Sequential()    
+    model.add(Dense(24, activation='relu', input_shape=states))
+    model.add(Dense(24, activation='relu'))
+    model.add(Dense(actions, activation='linear'))
+    return model
+
+
+def build_agent(model, actions):
+    policy = BoltzmannQPolicy()
+    memory = SequentialMemory(limit=50000, window_length=1)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy, 
+                  nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
+    return dqn
+
 class Application_Env(Env):
     
     def __init__(self,action_value_list,utility, action_dictionary, actions_keys:list):
@@ -161,21 +177,6 @@ class Application_Env(Env):
         return self.utility, self.seen_actions, self.seen_action_keys
 
 
-def build_model(states, actions):
-    model = Sequential()
-    model.add(Dense(units=24, activation="relu", input_shape=states))
-    model.add(Dense(units=24, activation="relu"))
-    model.add(Dense(units=actions, activation="linear"))
-    return model
-
-def build_agent(model, actions):
-    policy = BoltzmannQPolicy()
-    memory = SequentialMemory(limit=10000, window_length=1)
-    dqn = DQNAgent(model=model, memory=memory, policy=policy, 
-                  nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
-    return dqn
-
-
 
 int_act = initial_state(a)
 num_of_actions = 2
@@ -202,20 +203,13 @@ for episode in range(episodes+1):
 
 actions = my_env.action_space.n
 states = my_env.observation_space.shape
-print(states)
-    
 
-model = build_model(states=states, actions=actions)
+model = build_model(states, actions)
+
 model.summary()
-
 
 dqn = build_agent(model, actions)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+dqn.fit(my_env, nb_steps=50000, visualize=False, verbose=1)
 
-print("Training in the environment.......")
-
-dqn.fit(my_env, nb_steps=100, visualize=False, verbose=1)
-
-scores = dqn.test(my_env, nb_episodes=100, visualize=False)
-print(np.mean(scores.history['episode_reward']))
 
