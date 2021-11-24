811ee046f70356e2abea8d90bd91b2e5819a5d69
Pepi Paraskevoulakou <e.paraskevoulakou@unipi.gr>
2021-11-24 19:37:14 +0200

    new changes 4

--- env.py
+++ env.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
     my_env = Application_Env(action_value_list=action_value_list,utility=utility, 
                                     action_dictionary=action_dictionary, actions_keys=actions_keys)
 
-    episodes=10
+    episodes=2
     for episode in range(episodes+1):
             initial_utility = my_env.reset()
             done =False
@@ -202,6 +202,7 @@ if __name__ == "__main__":
 
     actions = my_env.action_space.n
     states = my_env.observation_space.shape
+    print(states)
     
 
     model = build_model(states=states, actions=actions)
