da33ca5f6fb0a394c22f1e4abfdb35598a8d9bdc
Pepi Paraskevoulakou <e.paraskevoulakou@unipi.gr>
2021-12-04 17:42:00 +0200

    changes to memory1

--- env.py
+++ env.py
@@ -275,8 +275,8 @@ if __name__ == "__main__":
             model.summary()
 
             dqn = build_agent(model, actions)
-            dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-            dqn.fit(my_env, nb_steps=100, visualize=False, verbose=1)
+            #dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+            #dqn.fit(my_env, nb_steps=100, visualize=False, verbose=1)
 
             scores = dqn.test(my_env, nb_episodes=100, visualize=False)
             print(np.mean(scores.history['episode_reward']))
