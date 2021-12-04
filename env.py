904a0666782f40690ef77cc9e3e437cb0a4bc074
Pepi Paraskevoulakou <e.paraskevoulakou@unipi.gr>
2021-12-04 17:45:39 +0200

    changes to memory2

--- env.py
+++ env.py
@@ -275,7 +275,7 @@ if __name__ == "__main__":
             model.summary()
 
             dqn = build_agent(model, actions)
-            #dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+            dqn.compile(Adam(lr=1e-3), metrics=['mae'])
             #dqn.fit(my_env, nb_steps=100, visualize=False, verbose=1)
 
             scores = dqn.test(my_env, nb_episodes=100, visualize=False)
